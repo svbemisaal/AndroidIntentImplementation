5b795f2692eb3ad952432fea58121213e969b2bf
Shubham Varshney <shubhamvarshney@Shubham-Varshney.local>
2020-07-27 21:04:40 +0530

    Moving from app to another app with a button click

--- app/src/main/java/com/example/textingestflowapp/MainActivity.kt
+++ app/src/main/java/com/example/textingestflowapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_favourite.*
 //import kotlinx.android.synthetic.main.activity_favorite.*
@@ -11,13 +13,32 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        val button:Button=findViewById(R.id.openNextApp)
+
+        button.setOnClickListener(View.OnClickListener() {
+
+                intent = Intent(Intent.ACTION_MAIN)
+
+                val manage = packageManager
+
+                intent = manage.getLaunchIntentForPackage("com.example.myfirstapp")
+                intent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+                startActivity(intent)
+
+
+        })
+
+
         moveToNext.setOnClickListener {
-            Log.i("Main Activity ","Th button has been clicked");
+            Log.i("Main Activity ","The button has been clicked");
 
             //Flashes message on the activity layout//
             Toast.makeText(  this,"The Button has been clicked by Shubham",Toast.LENGTH_SHORT).show();
@@ -57,6 +78,11 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(intent)
         }
+
+
+
+
+
     }
 
 
